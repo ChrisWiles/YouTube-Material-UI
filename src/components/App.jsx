39c08903b7790ba9c45e05853e1dd1c24c51c042
Christopher Wiles <chriswiles87@gmail.com>
2016-10-12 11:56:06 -0500

    add DarkTheme & NavBar

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import AppBar from 'material-ui/AppBar'
+import './App.css'
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <AppBar/>
+    )
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/App'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import './index.css'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const Wrapper = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <App/>
+  </MuiThemeProvider>
+)
+
+render(
+  <Wrapper/>, document.getElementById('root')
+)
