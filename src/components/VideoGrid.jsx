5a329c97a11afe242cfe031ec6a5152a98fc3c18
Christopher Wiles <chriswiles87@gmail.com>
2016-10-12 14:39:47 -0500

    create VideoGrid

---
+++ src/components/VideoGrid.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {GridList, GridTile} from 'material-ui/GridList'
+import IconButton from 'material-ui/IconButton'
+// import Subheader from 'material-ui/Subheader'
+import StarBorder from 'material-ui/svg-icons/toggle/star-border'
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    overflowY: 'auto',
+  },
+}
+
+const VideoGrid = ({tilesData}) => (
+  <div style={styles.root}>
+  {console.log(tilesData)}
+    <GridList
+      cellHeight={180}
+      style={styles.gridList}
+    >
+      {/* <Subheader>December</Subheader> */}
+      {tilesData.map((tile) => (
+        <GridTile
+          key={tile.id}
+          title={tile.title}
+          // subtitle={<span>by <b>{tile.author}</b></span>}
+          actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+        >
+          <img src={tile.img} role="presentation"/>
+        </GridTile>
+      ))}
+    </GridList>
+  </div>
+)
+
+export default VideoGrid
